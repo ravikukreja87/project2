2193db4dd382c9aa9cdf6134b161a16a41b0babf
ravikukreja87 <ravi8737@gmail.com>
2025-11-14 00:05:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import datetime
+y="Samarth"
+print(y)
+Birthdate1=input ("Enter your next birthdate ")
+format_string = "%Y-%m-%d"
+birth_date_1 = datetime.date.strptime(Birthdate1, format_string)
+currentdate=datetime.date.today()
+print(currentdate)
+print(type(currentdate))
+print(type(birth_date_1))
+answer=currentdate-birth_date_1
+print(f"You have your bithday in days",answer)
+x="Manupriya mam"
+print(x)
+Birthdate2=input("Enter your next birthdate ")
+format_string = "%Y-%m-%d"
+birth_date_2 = datetime.date.strptime(Birthdate2, format_string)
+currentdate=datetime.date.today()
+print(currentdate)
+print(type(currentdate))
+print(type(birth_date_2))
+answer=currentdate-birth_date_2
+z="Dia"
+print(z)
+Birthdate3=input("Enter your next birthdate ")
+format_string = "%Y-%m-%d"
+birth_date_3 = datetime.date.strptime(Birthdate2, format_string)
+currentdate=datetime.date.today()
+print(currentdate)
+print(type(currentdate))
+print(type(birth_date_3))
+answer=currentdate-birth_date_3
+a="Ravi"
+print(a)
+Birthdate4=input("Enter your next birthdate ")
+format_string = "%Y-%m-%d"
+birth_date_4 = datetime.date.strptime(Birthdate2, format_string)
+currentdate=datetime.date.today()
+print(currentdate)
+print(type(currentdate))
+print(type(birth_date_4))
+answer=currentdate-birth_date_4
+b="Sonakshi"
+print(b)
+Birthdate5=input("Enter your next birthdate ")
+Birthdate4=input("Enter your next birthdate ")
+format_string = "%Y-%m-%d"
+birth_date_5 = datetime.date.strptime(Birthdate2, format_string)
+currentdate=datetime.date.today()
+print(currentdate)
+print(type(currentdate))
+print(type(birth_date_5))
+answer=currentdate-birth_date_5
